489173fb97f84eabace13b96b546b5528dbef0fb
Qudus Oshinaike <llahsmine@gmail.com>
2024-10-31 12:20:36 +0100

    Functions

--- README.md
+++ README.md
@@ -5,13 +5,20 @@
 3.  As a matter of fact, they are the cornerstone of any programming language
 4.  Some of the key concepts one needs to understand include 👇👇👇
 
-        ✅ Function Basics
-        ✅ Function Expression
-        ✅ Arrow Function
-        ✅ Higher Order Function
-        ✅ Callback Function
-        ✅ Closure
-        ✅ Recursion
-        ✅ Immediately Invoked Function Expressions (IIFE) etc.
+    ✅ Function Basics
+    ✅ Function Expression
+    ✅ Arrow Function
+    ✅ Higher Order Function
+    ✅ Callback Function
+    ✅ Closure
+    ✅ Recursion
+    ✅ Immediately Invoked Function Expressions (IIFE) etc.
 
 5.  In this module, I wrote a sample code on how some of the aforementioned concepts can be implemented
+6.  The different programs in this perform diffent functions. These include:
+    ✅ Banking app (_using closure_): In this app, you can deposit, withraw and check balance
+    ✅ Countdown timer (_using closure_): This program counts down time
+    ✅ Prime number detector (_using arrow function_): This app helps detect and print any prime number found within any range
+    ✅ Multiplication program (_using recursion_): This program run any multiplication from 1 to 12 for any given number
+    ✅ Binary Converter (_using recursion_): This app converts any number to base 2 or binary
+7.  As regards the last program (Binary converter), even tho there is shoeter and better way to do it (i.e just running "_toString_" method on the given number) right, i decided to test my reasoning ability by using recursion.
--- func.js
+++ func.js
@@ -26,9 +26,9 @@ function recursion() {
 }
 recursion();
 
-//<==========================CLOSURE=======================>
+// //<==========================CLOSURE=======================>
 
-//Banking System=================>
+// //Banking App=================>
 
 //Outer Function
 function bankAccountBalance(initialBalance) {
@@ -59,7 +59,7 @@ console.log(accbal.checkBalance());
 console.log(accbal.deposit(530));
 console.log(accbal.withdraw(20));
 
-//Countdown Timer==================>
+// //Countdown Timer==================>
 //Outer function
 function countDown(count) {
   let start = count;
@@ -75,45 +75,8 @@ console.log(time());
 console.log(time());
 console.log(time());
 
-//Summation of numbers in 3X3 matrix====================>
-// Outer Function
-function addUp() {
-  const studentScores = [
-    [20, 30, 40],
-    [30, 40, 50],
-    [40, 50, 60],
-  ];
-  return {
-    // Object of Inner functions
-    first: function () {
-      const getScore = studentScores[0]; // This have access to the variable (studentScores) in the outer fucntion (addUp)
-      const grand = getScore.reduce((prev, curr) => {
-        return prev + curr;
-      });
-      return grand;
-    },
-    second: function () {
-      const getScore = studentScores[1]; // This have access to the variable (studentScores) in the outer fucntion (addUp)
-      grand = getScore.reduce((prev, curr) => {
-        return prev + curr;
-      });
-      return grand;
-    },
-    third: function () {
-      const getScore = studentScores[2]; // This have access to the variable (studentScores) in the outer fucntion (addUp)
-      grand = getScore.reduce((prev, curr) => {
-        return prev + curr;
-      });
-      return grand;
-    },
-  };
-}
-const total = addUp();
-
-console.log(total.first() + total.second() + total.third());
-
-//===========================FUNCTION EXPRESSION/ARROW FUNCTION==============================>
-//Prime Numbers============================>
+// // //===========================FUNCTION EXPRESSION/ARROW FUNCTION==============================>
+// // //Prime Numbers============================>
 
 const isPrimeNumber = (limit) => {
   for (let i = 2; i <= limit; i++) {
@@ -131,4 +94,116 @@ const isPrimeNumber = (limit) => {
 };
 isPrimeNumber(50);
 
-//To be continued
+//Muliplication program=================================>
+function timesTable(multiplier) {
+  let num = 0;
+  let multiplesOfMultiplier = [];
+  return {
+    arrayOfMultiples: function multiples() {
+      if (num > 12) {
+        return console.log(multiplesOfMultiplier);
+      }
+      const multiply = num * multiplier;
+      num++;
+      multiplesOfMultiplier.push(multiply);
+      multiples();
+    },
+    sumOfMultiples: function sum() {
+      const sumTotal = multiplesOfMultiplier.reduce((first, curr) => {
+        return (first += curr);
+      });
+      console.log(sumTotal);
+    },
+  };
+}
+const multiple = timesTable(3);
+multiple.arrayOfMultiples();
+multiple.sumOfMultiples();
+
+const studentScores = [[20, 30, 40], [30, 40, 50], [40, 50, 60], 50];
+function addUp() {
+  const first = studentScores.flat();
+  // const first = studentScores.flat().reduce((prev, curr) => {
+  //   return prev + curr;
+  // });
+  console.log(first);
+}
+addUp();
+
+let score = 0;
+
+let score2 = 0;
+let outerArr = null;
+function addArrayScores(arr) {
+  // declare var for length of array
+  if (outerArr === null) {
+    outerArr = arr;
+    const firstEl = outerArr.pop();
+    addArrayScores(firstEl);
+  }
+
+  if (arr.length > 0) {
+    const currItem = arr.pop();
+
+    score2 += currItem;
+    addArrayScores(arr);
+  } else if (outerArr.length > 0) {
+    const arrEl = outerArr.pop();
+    addArrayScores(arrEl);
+  }
+}
+
+const arrayOfScores = [
+  [20, 30, 40],
+  [30, 40, 50],
+  [40, 50, 60],
+];
+addArrayScores(arrayOfScores);
+console.log(score2);
+
+// find the total score
+// initialize a score variable
+
+for (const itemArray of arrayOfScores) {
+  console.log(itemArray);
+  if (Array.isArray(itemArray)) {
+    for (const item2 of itemArray) {
+      console.log(item2);
+      score = score + item2;
+      console.log(score);
+    }
+  } else {
+    score = score + itemArray;
+  }
+}
+
+/* for (let i = 0; i < arrayOfScores.length; i++) {
+  const item = arrayOfScores[i];
+} */
+
+console.log('this is the total score: ', score);
+
+//Conversion of numbers to base 2 (Binary)=========================>
+//Long cut===================>
+let r = 0;
+let store = [];
+function binaryGap(n) {
+  let Q = n;
+  return function binaryNum() {
+    if (Q <= 0) {
+      const binaryStr = store.reverse().join('');
+      if (store.length > 0) console.log(binaryStr);
+      return;
+    }
+    r = Q % 2;
+    Q = (Q - r) / 2;
+    store.push(r);
+    binaryNum();
+  };
+}
+const num = 5654;
+const binary = binaryGap(num);
+binary();
+
+//Shorter Cut===============================>
+console.log(num.toString(2));
